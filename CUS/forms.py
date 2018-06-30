44f15af3102416206fa78b95b3fcf0903ec1f7bd
Sarah Farnsworth-kumli <sarah@stormpath.com>
2018-06-30 12:59:36 -0700

    fixed routes, styled

--- CUS/forms.py
+++ CUS/forms.py
@@ -1,16 +1,38 @@
-from django.forms import ModelForm
+from django import forms
 from .models import cusapp
 
 
-class cusform(ModelForm):
+class cusform(forms.ModelForm):
     class Meta:
         model = cusapp
         fields = ['title', 'description', 'location']
+    title = forms.CharField(
+        label = 'Title',
+        max_length = 255,
+        required = True,
+        widget = forms.TextInput(
+            attrs = {'class': 'form-control', 'name': 'title'}
+        )
+    )
 
-class cusform(ModelForm):
-    class Meta:
-        model = cusapp
-        fields = ['title', 'description', 'location']
+    description = forms.CharField(
+        label = 'Body',
+        max_length = 255,
+        required = True,
+        widget = forms.TextInput(
+            attrs = {'class': 'form-control', 'name': 'description'}
+        )
+    )
+
+    location = forms.CharField(
+        label = 'Location',
+        max_length = 255,
+        required = True,
+        widget = forms.TextInput(
+            attrs = {'class': 'form-control', 'name': 'location'}
+        )
+    )
+
 
 
 
--- CUS/views.py
+++ CUS/views.py
@@ -2,37 +2,42 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .forms import cusform
 from .models import cusapp
 
-def homepage(request):
-    context = {
-
-    }
-    return render(request, 'cussapp/index2.html', context)
-
-
 def add_cusapp(request):
     if request.method == "POST":
         form = cusform(request.POST)
         if form.is_valid():
             cusapp_item = form.save(commit=False)
             cusapp_item.save()
-            return redirect('/cusapp/' + str(cusapp.id) + '/')
+            return redirect('maps/' + str(cusapp_item.id))
     else:
         form = cusform()
     return render(request, 'cusapp/index.html', {'form': form})
 
-def edit_cusapp(request, id=None):
-    item = get_object_or_404(Blog, id=id)
-    form = cusform(request.POST or None, instance=item)
-    if form.is_valid():
-        form.save()
-        return render(request, 'cusapp/index.html', {'form': form})
+def maps(request, id=None):
+    cus = get_object_or_404(cusapp, id=id)
+    return render(request, 'cusapp/maps.html', { 'cus': cus })
 
+# def login_reg(request):
+#     context = {
+#
+#     }
+#     return render(request, 'cusapp/login_reg', context)
+#
+# def homepage(request):
+#     context = {
+#     }
+#     return render(request, 'cusapp/index2.html', context)
 
-def cusapp(request):
-    context = {
-
-    }
-    return render(request, 'cusapp/index.html', context)
-
-def maps(request):
-    return render(request, 'cusapp/maps.html', {})
+# def edit_cusapp(request, id=None):
+#     item = get_object_or_404(Blog, id=id)
+#     form = cusform(request.POST or None, instance=item)
+#     if form.is_valid():
+#         form.save()
+#         return render(request, 'cusapp/index.html', {'form': form})
+#
+#
+# def cusapp(request):
+#     context = {
+#
+#     }
+#     return render(request, 'cusapp/index.html', context)
